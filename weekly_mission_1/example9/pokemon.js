09e0ee20689083088b6d5c3aa8f7efd0a0f79863
Buwar88 <buwar_emerica@hotmail.com>
2022-04-13 11:27:22 -0500

    nuevos archivos: weekly_mission_1/example1/main.js 	nuevos archivos: weekly_mission_1/example10/main.js 	nuevos archivos: weekly_mission_1/example10/package.json 	nuevos archivos: weekly_mission_1/example10/pokemon.js 	nuevos archivos: weekly_mission_1/example2/logger.js 	nuevos archivos: weekly_mission_1/example2/main.js 	nuevos archivos: weekly_mission_1/example3/logger_1.js 	nuevos archivos: weekly_mission_1/example3/logger_2.js 	nuevos archivos: weekly_mission_1/example3/main.js 	nuevos archivos: weekly_mission_1/example4/logger.js 	nuevos archivos: weekly_mission_1/example4/main.js 	nuevos archivos: weekly_mission_1/example5/logger.js 	nuevos archivos: weekly_mission_1/example5/main.js 	nuevos archivos: weekly_mission_1/example6/logger.js 	nuevos archivos: weekly_mission_1/example6/main.js 	nuevos archivos: weekly_mission_1/example6/patcher.js 	nuevos archivos: weekly_mission_1/example7/logger.js 	nuevos archivos: weekly_mission_1/example7/main.js 	nuevos archivos: weekly_mission_1/example7/main_2.js 	nuevos archivos: weekly_mission_1/example7/main_module.js  	nuevos archivos: weekly_mission_1/example7/package.json 	nuevos archivos: weekly_mission_1/example8/logger.js 	nuevos archivos: weekly_mission_1/example8/main.js 	nuevos archivos: weekly_mission_1/example8/main_2.js 	nuevos archivos: weekly_mission_1/example8/package.json 	nuevos archivos: weekly_mission_1/example9/main.js 	nuevos archivos: weekly_mission_1/example9/pokemon.js

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,14 @@
+class Pokemon {
+    constructor (name) {
+      this.name = name
+    }
+
+sayHello(message){
+    console.log(`Mi Pokemon ${this.name} te saluda!!`)
+}
+sayMessage(message){
+    console.log(`Mi Pokemon ${this.name} dice: ${message}`)
+}
+
+}
+module.exports = Pokemon
